6dc2b5f36258ed0b1da59836dfad2c306c671560
ChampionYusuf1 <championyusuf1@gmail.com>
2022-07-20 14:12:19 -0500

    Added a new page and a button to make it possible to inbewteen activities in hope of making

--- app/src/main/java/com/example/manualmasjidfinderjava/Activity2test.java
+++ app/src/main/java/com/example/manualmasjidfinderjava/Activity2test.java
@@ -2,15 +2,29 @@ package com.example.manualmasjidfinderjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Activity2test extends AppCompatActivity {
+    private Button firstactivityButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity2test);
+        firstactivityButton = findViewById(R.id.buttonformainactivity);
+
+        firstactivityButton.setOnClickListener((View.OnClickListener)v-> openActivity3());
 
 
+
+
+    }
+    public View.OnClickListener openActivity3() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        return null;
     }
 }
